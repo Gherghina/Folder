e663ab957919e8ab7426631316a213f098f537de
Gherghina <mar_gherasim@yahoo.com>
2019-09-11 20:59:05 +0300

    bla bla

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello FastTrack Word");
+
+	// Tipuri de date
+        String building = "Block";
+                String color = "White with yellow";
+                        byte floors = 3;
+                        float height = 25.6f;
+                        char purpouse
+
+
+    }
+}
